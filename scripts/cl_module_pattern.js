1ed5ee5fb677bc59ffcaea2c0f2f2bbd603cb698
Nazie Sharid <nazie.sh4rid@gmail.com>
2025-08-27 05:09:45 +0100

    Once Polyfill

--- scripts/cl_module_pattern.js
+++ scripts/cl_module_pattern.js
@@ -33,12 +33,19 @@ let isSubscribed = subscribe()
 document.getElementById('sub').addEventListener('click', () => isSubscribed())
 
 // Once Polyfill
-function oncuechange(func, context) {
+function once(func, context) {
   let ran
 
   return function () {
     if (func) {
-      ran = func.apply(context || this)
+      ran = func.apply(context || this, arguments)
+      func = null
     }
+
+    return ran
   }
 }
+const hello = once((a, b) => console.log('hello', a, b))
+hello(1, 2)
+hello(1, 3)
+hello(1, 2)
